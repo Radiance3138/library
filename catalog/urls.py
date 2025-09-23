09ddc7a3689909ffd2be1d45ae42562e6e811799
mjaved <mj.1963.2006@gmail.com>
2025-09-23 09:54:36 +0500

    Create homepage of the app

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('books-list/', views.books_list_view, name='books_list'),
+    path('authors-list/', views.authors_list_view, name='authors_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,42 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre
 
 def index(request):
     """View function for home page of site."""
 
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    num_genres = Genre.objects.count()
+
+    the_in_title_count = Book.objects.filter(title__icontains='The').count()
+
+
+
     context = {
-        'title': 'Home',
-        'header': 'Hello, World!',
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'the_in_title_count': the_in_title_count,
     }
     return render(
         request,
         'catalog/index.html', context=context
     )
 
-# Create your views here.
+def books_list_view(request):
+    books = Book.objects.all()
+    return render(request, 'catalog/books_list.html', {'books': books})
+
+def authors_list_view(request):
+    authors = Author.objects.all()
+    return render(request, 'catalog/authors_list.html', {'authors': authors})
+
+def about(request):
+    return render(request, 'catalog/about.html')
